7e8fffff5fc1f84548a50a37c0f704cd398b94ed
Herbert Li <2729415988@qq.com>
2025-10-08 17:32:36 +0800

    完成鸭血粉丝汤制作逻辑

---
+++ Assets/Scripts/Data/IngredientComponent.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+/// <summary>
+/// 挂在食材上的组件，标识其数据
+/// </summary>
+public class IngredientComponent : MonoBehaviour
+{
+    public IngredientSO ingredientData;
+    public ProxyDragTag proxyTag; 
+}
